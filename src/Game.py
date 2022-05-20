03c9d214d9a0542425baf17658ea790c7a8c1753
alexshhadeh <alexsh2001@hotmail.com>
2022-05-20 15:07:33 +0200

    393594: Refactor: combine branches

--- src/Game.py
+++
@@ -1,8 +0,0 @@
-from Maze import Maze
-
-if __name__ == '__main__':
-    # maze6 = Maze("../maps/case-6.txt")
-    # maze6.print_maze()
-
-    maze1 = Maze("../maps/case-1.txt")
-    maze1.print_maze()
