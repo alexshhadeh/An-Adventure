4ea8860571870b730d290cde51d088672c1fb605
alexshhadeh <alexsh2001@hotmail.com>
2022-05-20 13:58:07 +0200

    393508: Protected the path finding algorithm against test cases and made it PEP8 compliant

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-rawMaze = [
-    ['XXXXX'],
-    ['---XX'],
-    ['XX-XX'],
-    ['XX---'],
-    ['XX-XX']
-]
+rawMaze = [['XXXXX'], ['---XX'], ['XX-XX'], ['XX---'], ['XX-XX']]
 
 
 class Maze:
-    def __init__(self, rawMaze, startingPoint) -> None:
+
+    def __init__(self, rawMaze, startingPoint):
         self.maze = rawMaze
-        for i, v in enumerate(self.maze):
+        for (i, v) in enumerate(self.maze):
             self.maze[i] = list(v[0])
         self.startingPoint = startingPoint
         self.branches = []
@@ -22,12 +17,17 @@ class Maze:
             self.createdNewBranch = False
             for path in self.branches:
                 self.findPath(path)
-        if len(self.branches)==0:
-        	self.branches.append(onePath)
+        if len(self.branches) == 0:
+            self.branches.append(onePath)
+        for path in self.branches:
+            if self.isThereAnExit(path[len(path) - 1]) is False:
+                self.branches.remove(path)
         return self.branches
 
     def findShortestPath(self):
         self.findAllPaths()
+        if len(self.branches) == 0:
+            return []
         shortest = self.branches[0]
         for path in self.branches:
             if len(path) < len(shortest):
@@ -35,11 +35,15 @@ class Maze:
         return shortest
 
     def findPath(self, path):
-        currentPoint = path[len(path)-1]
+        currentPoint = path[len(path) - 1]
         foundPoints = self.filterPoints(self.findPoints(currentPoint), path)
 
+        for point in foundPoints:
+            if self.isInMaze(point) is False:
+                return path
+
         if len(foundPoints) > 1:
-            for _, point in enumerate(foundPoints, 1):
+            for (_, point) in enumerate(foundPoints, 1):
                 if self.isInMaze(point):
                     branch = list(path)
                     branch.append(point)
@@ -50,47 +54,56 @@ class Maze:
             if self.isInMaze(point):
                 path.append(point)
                 return self.findPath(path)
+
         return path
 
-    """def findPath(maze, path):
-		currentPoint=path[len(path)-1]
-		foundPoints=filterPoints(findPoints(maze, currentPoint), path)
-		for point in foundPoints:
-			if isInMaze(point, maze):
-				path.append(point)
-				return findPath(maze, path)
-		return path"""
+    def isThereAnExit(self, point):
+        for point in self.findAllPossiblePoints(point):
+            if self.isInMaze(point) is False:
+                return True
+        return False
 
     def isInMaze(self, point):
-        if point['x'] < 0 or point['y'] < 0:
-            return False
-        elif point['x'] > len(self.maze)-1 or point['y'] > len(self.maze)-1:
+        if (point['x'] < 0 or point['y'] < 0 or
+           point['x'] > len(self.maze) - 1 or
+           point['y'] > len(self.maze) - 1):
             return False
         else:
             return True
 
     def filterPoints(self, points, path):
-        filteredPoints = [point for point in points if point not in path]
+        filteredPoints = [point for point in points if point
+                          not in path]
         return filteredPoints
+
+    def findAllPossiblePoints(self, startingPoint):
+        directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        foundPoints = []
+        for direction in directions:
+            checkedPoint = {'x': startingPoint['x'] + direction[0],
+                            'y': startingPoint['y'] + direction[1]}
+            foundPoints.append(checkedPoint)
+        return foundPoints
 
     def findPoints(self, startingPoint):
         directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
         foundPoints = []
         for direction in directions:
-            checkedPoint = {
-                'x': startingPoint['x']+direction[0],
-                'y': startingPoint['y']+direction[1]
-            }
+            checkedPoint = {'x': startingPoint['x'] + direction[0],
+                            'y': startingPoint['y'] + direction[1]}
             try:
                 pointOnMap = self.maze[checkedPoint['x']][checkedPoint['y']]
                 if self.pointIsAccessible(pointOnMap):
+                    foundPoints.append(checkedPoint)
+                elif (checkedPoint['y'] == -1 and
+                      startingPoint != {'x': 1, 'y': 0}):
                     foundPoints.append(checkedPoint)
             except IndexError:
                 foundPoints.append(checkedPoint)
         return foundPoints
 
     def pointIsAccessible(self, pointOnMap):
-        if(pointOnMap == "-"):
+        if pointOnMap == '-':
             return True
         return False
 
